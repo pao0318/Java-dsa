e835e4c2a8b07c0b3ec3321e4fe35422f0c9b83d
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-04-05 14:06:41 +0530

    Add files via upload

---
+++ ddd.java
@@ -0,0 +1,9 @@
+
+class Main{
+    public static void main(String args[]) throws Exception{
+        System.out.println("Hello World");
+        int a=5;
+        int b=7;
+        System.out.println(a+b);
+    }
+}
