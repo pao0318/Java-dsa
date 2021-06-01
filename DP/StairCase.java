d566760444cb856bb0bdfc90b0cc9e6d15c376b7
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-01 17:30:42 +0530

    Create StairCase.java

---
+++ DP/StairCase.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]) throws Exception{
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int cp=countPathTab(n);
+        System.out.println(cp);
+    }
+    public static int countPathTab(int n){
+        int[] arr=new int[n+1];
+        arr[0]=1;
+        for(int i=1;i<n+1;i++){
+            if(i==1)
+                arr[i]=arr[i-1];
+            
+            else if(i==2)
+                arr[i]=arr[i-1]+arr[i-2];                
+            
+            else
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        }
+        return arr[n];
+        
+    }
+}
