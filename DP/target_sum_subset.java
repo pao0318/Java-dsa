da1f0e0bc5398cf4d81dfd6dc115347da785b271
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-02 21:30:14 +0530

    Create target_sum_subset.java

---
+++ DP/target_sum_subset.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    
+    int [][]arr=new int[n];
+    for(int i=0;i<arr.length;i++){
+        arr[i]=sc.nextInt();
+    }
+    int k=sc.nextInt();
+    boolean [][] dp=new boolean[n+1][k+1];
+        for(int i=0;i<dp.length;i++){
+            for(int j=0;j<dp[0].length;j++){
+                if(i==0&&j==0)
+                    dp[i][j]=true;
+                else if(i==0)
+                    dp[i][j]=false;
+                else if(j==0)
+                    dp[i][j]=true;
+                else{
+                    if(dp[i-1][j]==true)
+                        dp[i][j]=true;
+                    else
+                        int val=arr[i-1];
+                        if(val>=j){
+                            if(dp[i-1][j-val]==true)
+                                dp[i][j]=true;
+                        }
+                }
+            }
+        }
+    }
+}
