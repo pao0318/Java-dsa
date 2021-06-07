82e87dd54bddc1d1bcafcf8fe4d8d34ed6c318e0
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-07 14:08:42 +0530

    Update kth_smallest.java

--- HEAP/kth_smallest.java
+++ HEAP/kth_smallest.java
@@ -1,7 +1,7 @@
 class Solution{
     public static int kthSmallest(int[] arr, int l, int r, int k) {
         PriorityQueue<Integer> heap=new PriorityQueue<>();
-        for(int i=0;i<r;i++)
+        for(int i=0;i<=r;i++)
             heap.add(arr[i]);
             
         if(r==0)
