8ed1aebe880ec69c6573d51e9f3c479e8bb03923
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-07 13:44:35 +0530

    only for base cse

---
+++ HEAP/kth_smallest.java
@@ -0,0 +1,16 @@
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) {
+        PriorityQueue<Integer> heap=new PriorityQueue<>();
+        for(int i=0;i<r;i++)
+            heap.add(arr[i]);
+            
+        if(r==0)
+            return 0;
+            
+        for(int i=0;i<k-1;i++)
+            heap.poll();
+    
+    return heap.peek();
+    }
+    
+}
