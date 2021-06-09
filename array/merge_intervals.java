2045feff8cd9a79648cdd0cc88deb495f920faca
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-09 17:05:18 +0530

    Update merge_intervals.java

--- array/merge_intervals.java
+++ array/merge_intervals.java
@@ -1,7 +1,11 @@
+// import java.util.*;
 class Solution {
     public int[][] merge(int[][] intervals) {
+//         Sorting array inreasing order
         Arrays.sort(intervals, (a,b)->Integer.compare(a[0],b[0]));
+//         Creating linkedlist as size of output will be unkown
         LinkedList<int[]> list=new LinkedList<>();
+//         Now for every linkedlist it will check the original arrays last element
         for(int[] interval:intervals){
             if(list.isEmpty()|| list.getLast()[1]<interval[0])
                 list.add(interval);
