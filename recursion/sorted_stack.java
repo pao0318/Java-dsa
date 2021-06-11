ca739bc12a699c3566eae00ec1fabb2e15c195f5
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-11 21:27:07 +0530

    Create sorted_stack.java

---
+++ recursion/sorted_stack.java
@@ -0,0 +1,52 @@
+import java.util.*;
+public class Main{
+    static int sort(Stack<Integer> s){
+        if(!s.isEmpty()){
+            int x=s.pop();
+            sort(s);
+            pushback(s,x);
+        }
+    static void pushback(Stack<Integer>s, int x){
+    }    
+        if(s.isEmpty()||x>s.peek()){
+            s.push(x);
+            return;
+        }
+        int temp=s.pop();
+        pushback(s,x);
+        s.push(temp);
+        
+    }
+    // printing
+    static void printStack(Stack<Integer> s)
+    {
+        ListIterator<Integer> lt = s.listIterator();
+ 
+        // forwarding
+        while (lt.hasNext())
+            lt.next();
+ 
+        // printing from top to bottom
+        while (lt.hasPrevious())
+            System.out.print(lt.previous() + " ");
+    }
+    
+    public static void main(String args[]) throws Exception{
+        Scanner sc=new Scanner(System.in);
+        Stack<Integer> s = new Stack<>();
+        s.push(30);
+        s.push(-5);
+        s.push(18);
+        s.push(14);
+        s.push(-3);
+         System.out.println(
+            "Stack elements before sorting: ");
+        printStack(s);
+ 
+        sort(s);
+ 
+        System.out.println(
+            " \n\nStack elements after sorting:");
+        printStack(s);
+    }
+}
