d4b44ce053076e5d68709972b8241926d40dc3cd
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-11 14:33:35 +0530

    Update 1to_n.java

--- recursion/1to_n.java
+++ recursion/1to_n.java
@@ -1,8 +1,10 @@
 import java.util.*;
 public class Main{
     static void print(int n){
-        if(n==1)
+        if(n==1){
+            System.out.print(n);
             return;
+        }
         print(n-1);
         System.out.print(n);
 }
