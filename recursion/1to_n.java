8c93fdc41e11fb43304cf621275cef91379123c8
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-11 14:32:08 +0530

    1 to n recursion

---
+++ recursion/1to_n.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    static void print(int n){
+        if(n==1)
+            return;
+        print(n-1);
+        System.out.print(n);
+}
+
+public static void main(String args[]) throws Exception{
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    print(n);
+}
+}
