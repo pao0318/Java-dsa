e7bda4b43043bf0b4d5b74531dfc076f6d28f005
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-06-21 14:28:43 +0530

    Create balaned_parenthesis.java

---
+++ recursion/balaned_parenthesis.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[])throws Exception{
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        char [] str=new char[2*n];
+        solve(str,n);
+    }
+    static void solve(char str[], int n){
+        if(n>0){
+            printparenthesis(str,n,n,0);
+            return;
+        }
+    }
+    
+    static void printparenthesis(char str[], int open,int close,int pos){
+        if(open==0&&close==0){
+            for(int i=0;i<str.length;i++)
+                System.out.print(str[i]);
+            System.out.println();
+            return;
+        }
+        else{
+            if(open<close){
+                str[pos]=')';
+                printparenthesis(str,open,close-1,pos+1);
+            }
+            if(open!=0){
+                str[pos]='(';
+                printparenthesis(str,open-1,close,pos+1);
+            }
+        }
+        return;
+    }
+}
