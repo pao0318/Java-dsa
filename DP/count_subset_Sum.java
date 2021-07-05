21f9cc91fc72915f778949627fe701fda91b696e
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-07-05 21:32:28 +0530

    Update count_subset_Sum.java

--- DP/count_subset_Sum.java
+++ DP/count_subset_Sum.java
@@ -1,54 +1,34 @@
-// Java implementation of the approach
 import java.util.*;
-
-class GFG
-{
-static int maxN = 20;
-static int maxSum = 50;
-static int minSum = 50;
-static int base = 50;
-
-// To store the states of DP
-static int [][]dp = new int[maxN][maxSum + minSum];
-static boolean [][]v = new boolean[maxN][maxSum + minSum];
-
-// Function to return the required count
-static int findCnt(int []arr, int i,
-				int required_sum, int n)
-{
-	// Base case
-	if (i == n)
-	{
-		if (required_sum == 0)
-			return 1;
-		else
-			return 0;
-	}
-
-	// If the state has been solved before
-	// return the value of the state
-	if (v[i][required_sum + base])
-		return dp[i][required_sum + base];
-
-	// Setting the state as solved
-	v[i][required_sum + base] = true;
-
-	// Recurrence relation
-	dp[i][required_sum + base] =
-		findCnt(arr, i + 1, required_sum, n) +
-		findCnt(arr, i + 1, required_sum - arr[i], n);
-	return dp[i][required_sum + base];
+public class Main{
+    public static void main(String args[]) throws Exception{
+        Scanner sc=new Scanner(System.in);
+        int arr[]={2,3,5,6,8,10};
+        int k=10;
+        int ans=solve(arr,k);
+        System.out.println(ans);
+        
+    }
+    public static int solve(int[]arr,int k){
+        int [][]dp=new int[arr.length+1][k+1];
+        
+        for(int i=0;i<arr.length+1;i++){
+            for(int j=0;j<k+1;j++){
+                if(i==0)
+                    dp[i][j]=0;
+                if(j==0)
+                    dp[i][j]=1;
+            }
+        }
+        
+        for(int i=1;i<arr.length+1;i++){
+            for(int j=1;j<k+1;j++){
+                if(arr[i-1]<=j)
+                    dp[i][j]=dp[i-1][j]+dp[i-1][j-arr[i-1]];
+                else    
+                    dp[i][j]=dp[i-1][j];
+                
+            }
+        }
+        return dp[arr.length][k];
+    }
 }
-
-// Driver code
-public static void main(String []args)
-{
-	int arr[] = { 3, 3, 3, 3 };
-	int n = arr.length;
-	int x = 6;
-
-	System.out.println(findCnt(arr, 0, x, n));
-}
-}
-
-
