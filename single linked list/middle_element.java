7f70410dac5c4af136b81f6f72ed07c1d11aff0c
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-03 14:59:48 +0530

    Create middle_element.java

---
+++ single linked list/middle_element.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+/**
+ * class LLNode {
+ *   int val;
+ *   LLNode next;
+ * }
+ */
+class Solution {
+    public int solve(LLNode node) {
+         LLNode slow=node;
+         LLNode fast=node;
+         if(node==null)
+          return 0;
+          // if(fast.next.next==null)
+          //   return slow.next.val;
+         while(fast!=null && fast.next!=null){
+           slow=slow.next;
+           fast=fast.next.next;
+         }
+         return slow.val;
+
+    
+
+    }
+}
