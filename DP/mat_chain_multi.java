507391755ad447134ca46d3bda087c206e1000c8
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-20 23:32:25 +0530

    Update mat_chain_multi.java

--- DP/mat_chain_multi.java
+++ DP/mat_chain_multi.java
@@ -15,7 +15,7 @@ public class Main{
             return 0;
         int min=Integer.MAX_VALUE;
         for(int k=i;k<j;k++){
-            int temp=solve(arr,i,k)+solve(arr,k+1,j)+arr[i-1]*arr[k]*arr[j];
+            int temp=solve(arr,i,k)+solve(arr,k+1,j) + (arr[i-1]*arr[k]*arr[j]);
             min=Math.min(min,temp);
         }    
         return min;
