9ccc3e91879567987f2ba5000f0093b5f254a9a2
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-21 12:50:08 +0530

    Update permut_coeff.java

--- DP/permut_coeff.java
+++ DP/permut_coeff.java
@@ -27,3 +27,23 @@ public class Main{
         System.out.println(permute(x,y));
     }
 }
+// WITHOUT DP in O(N) space and O(1) complexity
+import java.util.*;
+public class Main{
+    static int permute(int u, int v){
+        int []fact=new int[u+1];
+        // base case:
+        fact[0]=1;
+        for(int i=1;i<u+1;i++)
+            fact[i]=i*fact[i-1];
+        
+        return fact[u]/fact[u-v];    
+        
+            
+    }
+    public static void main(String args[]) throws Exception{
+        int x=10;
+        int y=2;
+        System.out.println(permute(x,y));
+    }
+}
