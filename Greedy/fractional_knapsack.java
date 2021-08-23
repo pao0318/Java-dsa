891f93bd095e4ed7247d81f7a06d90cce92b51a6
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-23 20:28:20 +0530

    Update fractional_knapsack.java

--- Greedy/fractional_knapsack.java
+++ Greedy/fractional_knapsack.java
@@ -54,7 +54,84 @@ return maxValue;
 
 
 
-// OR
- Vector<pair<double,int>> v=new Vector<pair<double,int>>();
-        for(int i=0;i<n;i++){
-            double x=(arr[i].value*1.0)/
+// OR not working but still
+ import java.util.Arrays; 
+import java.util.Comparator; 
+  
+public class Main  
+{ 
+    // function to get maximum value attainable 
+    private static double getMaxValue(int[] weights, int[] val, 
+                                      int capacity) 
+    { 
+        WeightValue[] weightValue = new WeightValue[weights.length]; 
+        // Populate with the values
+        for (int i = 0; i < weights.length; i++) { 
+            weightValue[i] = new WeightValue(weights[i], val[i]); 
+        } 
+  
+        // sorting items by value/weight ratio; 
+        Arrays.sort(weightValue, new Comparator() { 
+            
+            public int compare(WeightValue o1, WeightValue o2) 
+            { 
+                return o2.ratio.compareTo(o1.ratio); 
+            } 
+        }); 
+  
+        double totalValue = 0d; 
+  
+        for (WeightValue i : weightValue) { 
+  
+            int curWeight = (int)i.weight; 
+            int curValue = (int)i.value; 
+  
+            if (capacity - curWeight >= 0)  
+            { 
+                // this weight can be picked while 
+                capacity = capacity - curWeight; 
+                totalValue += curValue; 
+            } 
+            else 
+            { 
+                // item cant be picked whole 
+                double fraction 
+                    = ((double)capacity / (double)curWeight); 
+                totalValue += (curValue * fraction); 
+                capacity 
+                    = (int)(capacity - (curWeight * fraction)); 
+                break; 
+            } 
+        } 
+  
+        return totalValue; 
+    } 
+  
+    // Weight value class 
+    static class WeightValue  
+    { 
+        Double ratio; 
+        double weight, value; 
+  
+        // item value function 
+        public WeightValue(int weight, int value) 
+        { 
+            this.weight = weight; 
+            this.value = value; 
+            ratio = new Double((double)value / (double)weight); 
+        } 
+    } 
+    
+    // Driver code 
+    public static void main(String[] args) 
+    { 
+        int[] weights = { 10, 20, 30 }; 
+        int[] values = { 60, 100, 120 }; 
+        int capacity = 50; 
+  
+        double maxValue = getMaxValue(weights, values, capacity); 
+        
+        System.out.println("Maximum Value Attainable - "
+                           + maxValue); 
+    } 
+}
