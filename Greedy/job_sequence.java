05678e1fbc80070ca35f87442b8e2922fe55c19f
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-24 12:39:59 +0530

    Update job_sequence.java

--- Greedy/job_sequence.java
+++ Greedy/job_sequence.java
@@ -111,7 +111,51 @@ class Solution
 
 
 
-
+// Ek aur approach
+class Solution
+{
+      class ItemComparator implements Comparator<Job>{
+        public int compare(Job o1, Job o2){
+            int n1=o1.profit;
+            int n2=o2.profit;
+            if(n1<n2) return 1;
+            else if(n1>n2) return -1;
+            else return 0;
+            
+        }
+    }
+    //Function to find the maximum profit and the number of jobs done.
+    int[] JobScheduling(Job arr[], int n)
+    {
+      Arrays.sort(arr,new ItemComparator());
+      
+       int max=Integer.MIN_VALUE;
+       for(int i=0;i<arr.length;i++)
+            max=Math.max(max,arr[i].deadline);
+        
+        int[] pro=new int[max+1];
+        Arrays.fill(pro,-1);
+        
+        int nJob=0;
+        int totProfit=0;
+        
+        for(int i=0;i<n;i++){
+            for(int j=arr[i].deadline;j>0;j--){
+                if(pro[j]==-1){
+                    pro[j]=i;
+                    nJob++;
+                    totProfit+=arr[i].profit;
+                    break;
+                }
+            }
+        }
+       
+        int res[]=new int[2];
+        res[0]=nJob;
+        res[1]=totProfit;
+        return res;
+    }
+}
 
 
 
