0925f896b504c18677b53db70ff803c4ca9ac0dc
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-25 11:06:16 +0530

    aaki kaam h

---
+++ Greedy/doubt_train.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Main
+{
+
+	public static void main(String[] args) {
+		int arr[][] ={{ 1000, 1030, 1,
+                      1010, 1020, 1,
+                      1000, 1020, 2,
+                      1130, 1230, 2,
+                      1200, 1230, 3,
+		               900, 1005, 1}};
+    System.out.println(maxTrains(arr));                  
+	}
+
+	static int maxTrains(int [][]arr){
+	    int n=arr.length;
+	    int m=3;
+	    
+	 
+	
+}
