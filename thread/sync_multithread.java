3ed8ab471c40ad8022e1a6308eaff400ae93758e
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-28 12:26:54 +0530

    Update sync_multithread.java

--- thread/sync_multithread.java
+++ thread/sync_multithread.java
@@ -55,3 +55,63 @@ public class Main{
         System.out.println("---Application Ended--");
     }
 }
+
+// Synchrnized all threads
+class Printer{
+    synchronized void printDoc(int num, String docname){
+        for(int i=1;i<=10;i++){
+            try{
+            Thread.sleep(500);
+            }catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.println(">>Printing "+docname+" "+i);
+        }
+    }
+}
+
+class Mythread extends Thread{
+    Printer pRef;
+    Mythread(Printer p){
+        pRef=p;
+    }
+    @Override
+    public void run(){
+        pRef.printDoc(10,"John");
+    }
+}
+class YourThread extends Thread{
+    Printer pRef;
+    YourThread(Printer p){
+        pRef=p;
+    }
+    @Override
+    public void run(){
+        pRef.printDoc(10,"PAOLI.pdf");
+    }
+}
+
+
+public class Main{
+    public static void main(String args[]){
+        System.out.println("---Application Started---");
+       
+        Printer printer=new Printer();
+        // printer.printDoc(10,"JAISHREERAM.PDF");
+        
+        // We have multiple threads working on same printer object
+        // If multiple threads are going to work on same songle onject we must Synchronize them
+        Mythread mRef=new Mythread(printer);
+        YourThread yRef=new YourThread(printer);
+        mRef.start();
+//         try{
+//             mRef.join(); 
+//             }catch(InterruptedException e){
+//                 e.printStackTrace();
+//             }
+        yRef.start();
+        
+        System.out.println("---Application Ended--");
+    }
+}
+
