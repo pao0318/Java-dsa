54f33ad231f5179ca66aae7056683759d15fb7cc
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-30 16:52:56 +0530

    Update earliest_unique.java

--- TRees/binary_search/earliest_unique.java
+++ TRees/binary_search/earliest_unique.java
@@ -19,3 +19,33 @@ class EarliestUnique {
         return -1;
     }
 }
+// Better
+import java.util.*;
+
+class EarliestUnique {
+    Set<Integer> set = null;
+    public EarliestUnique(int[] nums) {
+        set = new LinkedHashSet<>();
+        for (int ele : nums) {
+            if (set.contains(ele)) {
+                set.remove(ele);
+            } else {
+                set.add(ele);
+            }
+        }
+    }
+
+    public void add(int num) {
+        if (set.contains(num)) {
+            set.remove(num);
+        } else {
+            set.add(num);
+        }
+    }
+
+    public int earliestUnique() {
+        if (set.isEmpty())
+            return -1;
+        return set.iterator().next();
+    }
+}
