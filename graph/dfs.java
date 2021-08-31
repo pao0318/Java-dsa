07f8bf180af61268d9bf02a6bc4aaef64722ee4e
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-08-31 13:04:57 +0530

    Update dfs.java

--- graph/dfs.java
+++ graph/dfs.java
@@ -51,3 +51,53 @@ class Main{
  
     
 }
+// NEECHE WALA NHI HO RHA
+import java.util.*;
+public class Main{
+    static int V=4;
+    static ArrayList<LinkedList<Integer>> adj=new ArrayList<LinkedList<Integer>>(V);
+    static void addEdge(int u, int v){
+        adj.get(u).add(v);
+    }
+    
+    static void dfsutil(int v,boolean visited[]){
+        // Mark the current node as visited and print it
+        visited[v]=true;
+        System.out.println(v+" ");
+        // Recur for all the vertices adjacent to this
+        // vertex
+        Iterator<Integer> i=adj.get(v).listIterator();
+        while(i.hasNext()){
+            int n=i.next();
+            if(!visited[n])
+                dfsutil(n,visited);
+        }
+    }
+    
+    
+    static void dfs(int v){
+        boolean [] visited=new boolean[v];
+        dfsutil(v,visited);
+    }
+    
+    
+    // main function
+    public static void main(String args[]) throws Exception{
+        
+        for(int i=0;i<V;i++){
+            adj.add(new LinkedList<Integer>());
+        }
+        
+        addEdge(0, 1);
+        addEdge(0, 2);
+        addEdge(1, 2);
+        addEdge(2, 0);
+        addEdge(2, 3);
+        addEdge(3, 3);
+        
+        System.out.println(
+            "Following is Depth First Traversal "
+            + "(starting from vertex 2)");
+        dfs(2);  
+    }
+}
