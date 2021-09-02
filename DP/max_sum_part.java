77869ae90fcf02ab0d81b6bcfbe09ea06f103325
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-02 13:33:38 +0530

    Update max_sum_part.java

--- DP/max_sum_part.java
+++ DP/max_sum_part.java
@@ -3,18 +3,20 @@ import java.util.*;
 class Solution {
     public int solve(int[] nums, int k) {
         int n=nums.length;
-        int []dp=new int[n];
-        int max=Integer.MIN_VALUE;
-        dp[0]=nums[0];
-        for(int i=1;i<n;i++){
-            max=nums[i];
-            for(int j=i;j>i-k&&j>=0;j--){
-                max=Math.max(nums[j],max);
-                dp[i]=Math.max(dp[i],dp[j-1]+max*(i-j+1));
+        int []dp=new int[n+1];
+        int max;
+        // dp[0]=nums[0];
+        for(int i=1;i<=n;i++){
+            max=0;
+            for(int j=i-1;j>=i-k &&j>=0;j--){
+                max=Math.max(max,nums[j]);
+                dp[i]=Math.max(dp[i],dp[j]+max*(i-j));
                 
             }
         }
-        return dp[n-1];
+        return dp[n];
 
     }
+
 }
+
