0e58ab2ac1c4538518f218316cee4bbcdad5efca
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-03 13:55:22 +0530

    Update left_side_view.java

--- TRees/binary_search/left_side_view.java
+++ TRees/binary_search/left_side_view.java
@@ -36,3 +36,32 @@ class Solution {
         return list;
     }
 }
+// ArrayList
+class Solution {
+    public int[] solve(Tree root) {
+        if(root==null)
+            return new int[0];
+        Queue<Tree> pq=new LinkedList<>();
+        ArrayList<Integer> list=new ArrayList<>();
+        pq.add(root);
+        while(!pq.isEmpty()){
+            int s=pq.size();
+            for(int i=1;i<=s;i++){
+                Tree temp=pq.poll();
+                if(i==1)
+                    list.add(temp.val);
+                if(temp.left!=null)
+                    pq.add(temp.left);
+
+                if(temp.right!=null)
+                    pq.add(temp.right);        
+            }
+        }
+        int[]arr=new int[list.size()];
+        for(int i=0; i<list.size();i++){
+            arr[i]=list.get(i);
+        }
+        return arr;
+        
+    }
+}
