e63ef688c5828b9dbab922ca6c67c02b6524f9b4
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-04 00:54:27 +0530

    Update width.java

--- TRees/gfg/width.java
+++ TRees/gfg/width.java
@@ -1,23 +1,4 @@
-class Solution {
-    // Function to return the diameter of a Binary Tree.
-    int res=0;
-    int diameter(Node root) {
-        helper(root);
-        return res;
-    }
-    int helper(Node root){
-        if(root==null)
-            return 0;
-        
-        int l=helper(root.left);
-        int r=helper(root.right);
-        res=Math.max(res,l+r+1);
-        
-        return 1+Math.max(l,r);
-        
-        
-    }
-}
+
 // Scratch tree
 import java.util.*;
 class Node{
