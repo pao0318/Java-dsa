cfbfe4425d75f6f9b16b6436822cf7da0242012c
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-04 03:20:56 +0530

    Update delete_a_node.java

--- TRees/gfg/delete_a_node.java
+++ TRees/gfg/delete_a_node.java
@@ -1,3 +1,47 @@
+// Pepcoding
+public static Node remove(Node node, int data) {
+      if(node==null)
+        return null;
+    if(data>node.data){
+        node.right=remove(node.right,data);
+    }else if(data<node.data){
+        node.left=remove(node.left,data);
+    }else{
+        //work
+        if(node.left!=null && node.right!=null){
+            int lmax=Math.max(node.left);
+            node.data=lmax;
+            node.left=remove(node.left,lmax);
+            return node;
+            
+        }else if(node.left!=null){
+            return node.left;
+            
+        }else if(node.right!=null){
+            return node.right;
+            
+        }else{
+            return null;
+        }
+        return node;
+  }
+
+
+
+
+
+// Neeche me scratch se hai
+
+
+
+
+
+
+
+
+
+
+
 // Java program to delete element
 // in binary tree
 import java.util.LinkedList;
