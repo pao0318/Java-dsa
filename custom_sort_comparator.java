9f315cdd39d3b5bbcdfcc0f01f3af470714ce2d1
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-09 13:46:18 +0530

    Update custom_sort_comparator.java

--- custom_sort_comparator.java
+++ custom_sort_comparator.java
@@ -43,3 +43,51 @@ class Solution {
         }
     }
 }
+
+
+// OR
+import java.util.*;
+
+class Solution {
+    public int[] solve(int[] nums) {
+        Pair[]arr=new Pair[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            arr[i]=new Pair(nums[i],Integer.bitCount(nums[i]));
+        }
+        Sort s=new Sort();
+        Arrays.sort(arr,s);
+
+        int[]ans=new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            ans[i]=arr[i].x;
+        }
+        return ans;
+    }
+
+
+    class Pair{
+        int x;
+        int y;
+        public Pair(int x, int y){
+            this.x=x;
+            this.y=y;
+        }
+    }
+
+    // int binarize(int dig){
+    //     int c=0;
+    //     while(dig>0){
+    //         c+=dig&1;
+    //         dig>>=1;
+    //     }
+    //     return c;
+    // }
+    static class Sort implements Comparator<Pair>{
+        public int compare(Pair a, Pair b){
+            if(a.y==b.y)
+                return a.x-b.x;
+            return a.y-b.y;    
+        }
+    }
+}
