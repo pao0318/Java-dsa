c29ef0251778aeba413d5beaf7b7610a85852393
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-10 11:26:08 +0530

    Update bfsbasic.java

--- graph/bfsbasic.java
+++ graph/bfsbasic.java
@@ -22,3 +22,60 @@ class Solution
         return list;
     }
 }
+// scratch
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+   static class Edge {
+      int src;
+      int nbr;
+
+      Edge(int src, int nbr){
+         this.src = src;
+         this.nbr = nbr;
+        
+        }
+    }
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int vtcs=sc.nextInt();
+        ArrayList<Edge> []list=new ArrayList[vtcs];
+        for(int i=0;i<vtcs;i++)
+            list[i]=new ArrayList<>();
+            
+         int edges=sc.nextInt();
+         
+         for(int i=0;i<edges;i++){
+             int n1=sc.nextInt();
+             int n2=sc.nextInt();
+             list[n1].add(new Edge(n1,n2));
+            //  list[n2].add(new Edge(n2,n1));
+         }
+         int src=sc.nextInt();
+           bfs(src,vtcs,list);
+            
+    }
+    static void bfs(int src, int vtcs,ArrayList<Edge> []list){
+        boolean []visited=new boolean[vtcs];
+        Queue<Integer> pq=new LinkedList<>();
+        visited[src]=true;
+        pq.add(src);
+        while(!pq.isEmpty()){
+            src=pq.poll();
+            System.out.print(src+" ");
+            
+
+            while(list[src].nbr!=0){
+                int jai=list[src].nbr();
+                if(!visited[jai]){
+                    visited[jai]=true;
+                    pq.add(jai);
+                }
+            }
+        }
+    } 
+}
+    
+   
