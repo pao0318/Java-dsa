43796ee2a2bca896be9c21716a962c8eba93b6f9
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-16 21:17:24 +0530

    Create move_zeros.java

---
+++ array/move_zeros.java
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/move-zeroes/submissions/
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i=0;
+    for (int j = 0; j < nums.length; j++){
+        if (nums[j] != 0){
+      int temp = nums[i];
+      nums[i] = nums[j];
+      nums[j] = temp;
+      i++;
+    }
+  }
+}
+}
