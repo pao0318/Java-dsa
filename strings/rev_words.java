19a71903eef24c0bb571862f35c66b1254283461
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-17 12:39:29 +0530

    Update and rename rev_words,java to rev_words.java

--- strings/rev_words.java
+++ strings/rev_words.java
@@ -28,3 +28,12 @@ class Solution {
    
     
 }
+// OR
+  public String reverseWords(String s) {
+        String[] strings = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String str : strings) {
+            sb.append(new StringBuilder(str).reverse().append(" "));
+        }
+        return sb.toString().trim();
+    }
