94c1362468aac77cbab3bf90ab241562303e78c4
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-09-21 23:33:23 +0530

    Create reshape_array.java

---
+++ array/reshape_array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
+        int pro=(mat.length)*(mat[0].length);
+        
+        
+        int [][]res=new int[r][c];
+        
+        if(r*c!=pro)
+            return mat;
+        int l=0;
+        int k=0;
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(k<c)
+                    res[l][k++]=mat[i][j];
+                else{
+                    k=0;
+                    l++;
+                    res[l][k++]=mat[i][j];
+                }
+            }
+        }
+       
+        
+        return res;
+        
+        
+        
+    }
+}
