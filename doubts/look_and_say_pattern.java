ed8f86883ded354418d1d2fbd6033b147bd1bdbc
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-10-13 17:32:12 +0530

    solved!

--- doubts/look_and_say_pattern.java
+++ doubts/look_and_say_pattern.java
@@ -1,1 +1,28 @@
 // https://practice.geeksforgeeks.org/problems/implement-atoi/1#
+class Solution{
+    static String lookandsay(int n) {
+        if(n==1)
+            return "1";
+        if(n==2)
+            return "11";
+            
+        String res=lookandsay(n-1);
+        StringBuilder newres=new StringBuilder();
+        int count=1;
+        for(int i=1;i<res.length();i++){
+            if(res.charAt(i-1)!=res.charAt(i)){
+                newres.append(count);
+                newres.append(res.charAt(i-1));
+                count=1;
+            }
+            else
+                count++;
+            if(i==res.length()-1){
+                newres.append(count);
+                newres.append(res.charAt(i));
+            }
+        }
+        return newres.toString();
+        
+    }
+}
