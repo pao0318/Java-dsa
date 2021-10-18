8bda6978c81e8f849e9f6c32e5e7046744526801
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-10-18 17:51:45 +0530

    Update max_of_k_subarrays.java

--- queue/gfg/max_of_k_subarrays.java
+++ queue/gfg/max_of_k_subarrays.java
@@ -1,4 +1,37 @@
 // https://practice.geeksforgeeks.org/problems/maximum-of-all-subarrays-of-size-k3101/1#
+// YEH BETTER H
+class Solution
+{
+    //Function to find maximum of each subarray of size k.
+    static ArrayList <Integer> max_of_subarrays(int arr[], int n, int k)
+    {
+        // creating the max heap ,to get max element always
+        PriorityQueue<Integer> pq = new PriorityQueue<>(
+            Collections.reverseOrder());
+
+        ArrayList<Integer> res = new ArrayList<>();
+        int i = 0;
+        
+        for(;i<k;i++){
+            pq.add(arr[i]);
+        }
+        res.add(pq.peek());
+        
+        pq.remove(arr[0]);
+        
+        for(; i<n;i++){
+            pq.add(arr[i]);
+            res.add(pq.peek());
+            pq.remove(arr[i-k+1]);
+        }
+        return res;
+    }
+}
+
+
+
+
+// YE DEQUE
 class Solution
 {
     //Function to find maximum of each subarray of size k.
