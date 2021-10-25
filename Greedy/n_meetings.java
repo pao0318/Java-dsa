248991c74a4d9e90e3bd7c07bc6f2e5425e58965
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-10-25 20:00:59 +0530

    Update n_meetings.java

--- Greedy/n_meetings.java
+++ Greedy/n_meetings.java
@@ -1,38 +1,31 @@
-// https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1
+// https://practice.geeksforgeeks.org/problems/n-meetings-in-one-room-1587115620/1#
+
 class Solution 
 {
     //Function to find the maximum number of meetings that can
     //be performed in a meeting room.
     public static int maxMeetings(int start[], int end[], int n)
     {
-   HashMap<Integer , Integer> map = new HashMap<>();
-    for( int i =0 ; i < n ; i++){
-        if( !map.containsKey(start[i])){
-            map.put( start[i] , end[i]);
-            }
-        else{
-            int temp = Math.min(map.get(start[i]) , end[i]);
-            map.put( start[i] , temp);
-
-            }
-}
-    Arrays.sort(start);
-    int i = 1 ;
-    int end1 = map.get(start[0]);
-    int ans = 1;
-    while( i < n ){
-        if( end1 < start[i] ) {
-            ans++;
-            end1 = map.get(start[i]);
-            i++;
-}
-        else{
-            if( map.get( start[i]) < end1)
-                end1 = map.get(start[i]);
-                i++;
-                }
+        int[][]arr=new int[n][2];
+        int len=0;
+        int count=1;
+        for(int i=0;i<n;i++){
+            arr[i][0]=start[i];
+            arr[i][1]=end[i];
         }
-return ans;
+        Arrays.sort(arr,(a,b)-> Double.compare(a[1],b[1]));
+        int j=0;
+        for(int i=1;i<n;i++){
+            if(arr[i][0]>arr[j][1]){
+                count++;
+                j=i;
+            }
 
-}
+        }
+        return count;
+        
+        
+    }
+    
+
 }
