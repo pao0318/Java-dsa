88b20a55dd828cdf342cebf53656b6d28a21f33a
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-11-17 09:29:31 +0530

    Update jump_game2.java

--- Greedy/jump_game2.java
+++ Greedy/jump_game2.java
@@ -16,3 +16,25 @@ class Solution {
         return count;
     }
 }
+// DP approach
+
+class Solution {
+    public int jump(int[] nums) {
+        int n=nums.length;
+       int[]dp=new int[nums.length];
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[n-1]=0;
+        
+        for(int i=n-2;i>=0;i--){
+            int min=Integer.MAX_VALUE;
+            
+            for(int j=i+1;j<=Math.min(n-1,i+nums[i]);j++){
+                min=Math.min(dp[j],min);
+                if(min!=Integer.MAX_VALUE)
+                    dp[i]=1+min;
+            }
+        }
+        return dp[0];
+        
+    }
+}
