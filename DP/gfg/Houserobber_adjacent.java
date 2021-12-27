e9ea342ffc6fda74dd5b7f235a7a8c167641a27a
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-12-27 13:21:41 +0530

    Update Houserobber_adjacent.java

--- DP/gfg/Houserobber_adjacent.java
+++ DP/gfg/Houserobber_adjacent.java
@@ -1,18 +1,16 @@
-// https://leetcode.com/problems/house-robber/submissions/
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1)
-            return nums[0];
         if(nums.length==0)
             return 0;
         int[]dp=new int[nums.length];
+        if(nums.length==1)
+            return nums[0];
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
         
-        for(int i=2;i<nums.length;i++)
-            dp[i]=Math.max(dp[i-1], dp[i-2]+nums[i]);
-        
+        for(int i=2;i<nums.length;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+nums[i]);
+        }
         return Math.max(dp[nums.length-1],dp[nums.length-2]);
-        
     }
 }
