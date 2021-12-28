0829741431097d881c4be4246bded4e52c429524
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-12-28 20:15:04 +0530

    Update reverse_recursion.java

--- single linked list/reverse_recursion.java
+++ single linked list/reverse_recursion.java
@@ -1,25 +1,15 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     public int val;
- *     public ListNode next;
- *     ListNode(int x) { val = x; next = null; }
- * }
- */
-public class Solution {
-    ListNode head;
-    public ListNode reverseList(ListNode A) {
-        // ListNode head;
-        if(A.next==null){
-            head=A;
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null||head.next==null)
             return head;
-        }
-        reverseList(A.next);
-        ListNode b=A.next;
-        b.next=A;
-        A.next=null;
         
-        return head;
-            
+        ListNode curr=reverseList(head.next);
+        ListNode newhead=head.next;
+        newhead.next=head;
+        head.next=null;
+        
+        return curr;
+        
+        
     }
 }
