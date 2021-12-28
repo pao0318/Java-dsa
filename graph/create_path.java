9838c5e57eee956a09f98dd0e2a44478bfa63dcd
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-12-28 18:19:44 +0530

    Update create_path.java

--- graph/create_path.java
+++ graph/create_path.java
@@ -1,37 +1,39 @@
 // https://www.geeksforgeeks.org/graph-and-its-representations/
 import java.util.*;
-public class Main{
-    static void addEdge(ArrayList<ArrayList<Integer>>adj, int u, int v){
-        adj.get(u).add(v);
-        adj.get(v).add(u);
-        
+class Main{
+    
+    static void addEdge(ArrayList<ArrayList<Integer>>graph, int u, int v){
+        graph.get(u).add(v);
+        graph.get(v).add(u);
     }
+    
+    static void printGraph(ArrayList<ArrayList<Integer>>graph){
+        
+        for(int i=0;i<graph.size();i++){
+            System.out.println("List of "+i);
+            System.out.print("Head ->");
+            for(int j=0;j<graph.get(i).size();j++){
+                System.out.print(graph.get(i).get(j)+"->");
+            }
+            System.out.println(" ");
+        }
+    }
+    
     public static void main(String args[]) throws Exception{
-        // for 5 vertices
         int V=5;
-        ArrayList<ArrayList<Integer>> graph=new ArrayList<ArrayList<Integer>>(V);
+        ArrayList<ArrayList<Integer>>graph=new ArrayList<>(V);
+        
         for(int i=0;i<V;i++)
             graph.add(new ArrayList<Integer>());
             
-            addEdge(graph,0,1);
-            addEdge(graph,0,4);
-            addEdge(graph,1,2);
-            addEdge(graph,1,3);
-            addEdge(graph,1,4);
-            addEdge(graph,2,3);
-            addEdge(graph,3,4);
-            
-            printGraph(graph);
+        addEdge(graph,0,1);
+        addEdge(graph,0,4);
+        addEdge(graph,1,4);
+        addEdge(graph,1,2);
+        addEdge(graph,1,3);
+        addEdge(graph,2,3);
+        addEdge(graph,3,4);
         
-    }
-    static void printGraph(ArrayList<ArrayList<Integer>>adj){
-        for(int i=0;i<adj.size();i++){
-            System.out.println("Adjacency list of vertex"+i);
-            System.out.print("head");
-            for(int j=0;j<adj.get(i).size();j++)
-                System.out.print("->"+adj.get(i).get(j));
-            System.out.println();
-        }
-        
+        printGraph(graph);
     }
 }
