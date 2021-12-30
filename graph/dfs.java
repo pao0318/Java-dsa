8cebbaae3e5c642e641e4ea36f72c66dc9517a6a
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2021-12-31 00:58:30 +0530

    Update dfs.java

--- graph/dfs.java
+++ graph/dfs.java
@@ -1,103 +1,62 @@
+import java.io.*;
 import java.util.*;
-class Main{
-    private int V;
-    private LinkedList<Integer> adj[];
-    
-    Main(int v){
-        V=v;
-        adj=new LinkedList[v];
-        for(int i=0;i<v;++i)
-            adj[i]=new LinkedList();
-    }
-    
-    void addEdge(int u, int v){
-        adj[u].add(v);
-    }
-    
-    
-    void dfshelper(int v, boolean visited[]){
-        visited[v] = true;
-        System.out.print(v + " ");
-        
-        Iterator<Integer> itr=adj[v].listIterator();
-            while(itr.hasNext()){
-                int num=itr.next();
-                if(!visited[num])
-                    dfshelper(num,visited);
-            }
-    }
-    void DFS(int v){
-        boolean []visited=new boolean[V];
-         dfshelper(v,visited);
-        }
-    
-    
+class Main
+{
+	private int V; 
+	private LinkedList<Integer> adj[]; 
 
-    
-    // main function starts
-    public static void main(String args[]) throws Exception{
-        Main g=new Main(4);
-        g.addEdge(0,1);
-        g.addEdge(0,2);
-        g.addEdge(1,2);
-        g.addEdge(2,0);
-        g.addEdge(2,3);
-        g.addEdge(3,3);
-        System.out.println(
-            "Following is Depth First Traversal");
-  
-        g.DFS(0);
-    }
- 
-    
-}
-// NEECHE WALA NHI HO RHA
-import java.util.*;
-public class Main{
-    static int V=4;
-    static ArrayList<LinkedList<Integer>> adj=new ArrayList<LinkedList<Integer>>(V);
-    static void addEdge(int u, int v){
-        adj.get(u).add(v);
-    }
-    
-    static void dfsutil(int v,boolean visited[]){
-        // Mark the current node as visited and print it
-        visited[v]=true;
-        System.out.println(v+" ");
-        // Recur for all the vertices adjacent to this
-        // vertex
-        Iterator<Integer> i=adj.get(v).listIterator();
-        while(i.hasNext()){
-            int n=i.next();
-            if(!visited[n])
-                dfsutil(n,visited);
-        }
-    }
-    
-    
-    static void dfs(int v){
-        boolean [] visited=new boolean[v];
-        dfsutil(v,visited);
-    }
-    
-    
-    // main function
-    public static void main(String args[]) throws Exception{
-        
-        for(int i=0;i<V;i++){
-            adj.add(new LinkedList<Integer>());
-        }
-        
-        addEdge(0, 1);
-        addEdge(0, 2);
-        addEdge(1, 2);
-        addEdge(2, 0);
-        addEdge(2, 3);
-        addEdge(3, 3);
-        
-        System.out.println(
-            "Following is Depth First Traversal "
-            + "(starting from vertex 2)");
-        dfs(2);  
-    }
+	Main(int v)
+	{
+		V = v;
+		adj = new LinkedList[v];
+		for (int i=0; i<v; ++i)
+			adj[i] = new LinkedList();
+	}
+
+	void addEdge(int v,int w)
+	{
+		adj[v].add(w);
+	}
+
+
+
+	void BFS(int s){
+		boolean visited[] = new boolean[V];
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		visited[s]=true;
+		queue.add(s);
+
+		while (queue.size() != 0){
+			s = queue.poll();
+			System.out.print(s+" ");
+			Iterator<Integer> i = adj[s].listIterator();
+			while (i.hasNext())
+			{
+				int n = i.next();
+				if (!visited[n])
+				{
+					visited[n] = true;
+					queue.add(n);
+				}
+			}
+		}
+	}
+
+
+	public static void main(String args[])
+	{
+		Main g = new Main(4);
+
+		g.addEdge(0, 1);
+		g.addEdge(0, 2);
+		g.addEdge(1, 2);
+		g.addEdge(2, 0);
+		g.addEdge(2, 3);
+		g.addEdge(3, 3);
+
+		System.out.println("Following is Breadth First Traversal "+
+						"(starting from vertex 2)");
+
+		g.BFS(2);
+	}
 }
