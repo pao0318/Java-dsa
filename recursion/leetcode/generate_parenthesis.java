f86ede0344b049ed2bdf35c95d8b4113e14822c7
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-01-14 12:52:10 +0530

    Create generate_parenthesis.java

---
+++ recursion/leetcode/generate_parenthesis.java
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/generate-parentheses/
+import java.util.ArrayList;
+
+/*
+ * @lc app=leetcode id=22 lang=java
+ *
+ * [22] Generate Parentheses
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+
+    public void backtrack(List<String> list, String str, int open, int close, int max) {
+        if (str.length() == 2 * max) {
+            list.add(str);
+            return;
+        }
+
+        if (open < max)
+            backtrack(list, str + "(", open + 1, close, max);
+        if (close < open)
+            backtrack(list, str + ")", open, close + 1, max);
+    }
+
+}
