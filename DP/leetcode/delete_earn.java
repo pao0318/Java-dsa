b4505ca8808b4f1865577a45937970577a9e8cda
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-05-11 12:52:59 +0530

    Update delete_earn.java

--- DP/leetcode/delete_earn.java
+++ DP/leetcode/delete_earn.java
@@ -1,24 +1,22 @@
 // https://leetcode.com/problems/delete-and-earn/submissions/
 class Solution {
     public int deleteAndEarn(int[] nums) {
-      if (nums == null || nums.length == 0) {
+      if (nums == null || nums.length == 0) 
             return 0;
-        }
+        int inc=0;
+        int exc=0;
         
-        int dp[] = new int[10001]; 
-        for (int i = 0; i < nums.length; i++) {
-            dp[nums[i]] += nums[i]; // add them to dp  
-        }
-        int rob1=0;
-        int rob2=0;
-		
-        for (int i=0;i<dp.length;i++) {
-            int temp = Math.max(dp[i]+rob1, rob2); 
-            rob1=rob2;  
-            rob2=temp;
-        }
-        return rob2; 
-    }
+        int count[]=new int[10001];
+        for(int val:nums)
+            count[val]++;
         
-    
+        for(int i=0;i<10001;i++){
+            int ni=exc+count[i]*i;
+            int ne= Math.max(inc,exc);
+            inc=ni;
+            exc=ne;
+        }
+        return Math.max(inc,exc);
+        
+    }  
 }
