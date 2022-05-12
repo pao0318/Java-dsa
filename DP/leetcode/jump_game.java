ecb968cab918d201d53e19d81e475ea12ec278d3
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-05-12 12:16:52 +0530

    Update jump_game.java

--- DP/leetcode/jump_game.java
+++ DP/leetcode/jump_game.java
@@ -9,3 +9,20 @@ class Solution {
         return true;
     }
 }
+
+
+// dp
+class Solution {
+    public boolean canJump(int[] nums) {
+        int[]dp=new int[nums.length];
+    
+        dp[0]= nums[0];
+        for(int i=1;i<nums.length;i++){
+            if(i>dp[i-1])
+                return false;
+       dp[i] = Math.max(dp[i - 1], i + nums[i]);
+            
+        }
+      return true;
+    }
+}
