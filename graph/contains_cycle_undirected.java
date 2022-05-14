4bec05285b6834abc432fb510cd8e0f4b00662f9
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-05-14 23:54:56 +0530

    Update contains_cycle_undirected.java

--- graph/contains_cycle_undirected.java
+++ graph/contains_cycle_undirected.java
@@ -1,30 +1,40 @@
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
-        boolean visited[]=new boolean[V];
-        boolean ret=true;
-        for(int i=0;i<V;i++)
-            ret=ret&&dfs(adj,-1,i,visited);
-        return !ret;    
         
-    }
-    boolean dfs(ArrayList<ArrayList<Integer>> adj, int parent,int src,boolean[]visited){
-        if(visited[src])
-            return true;
+        boolean []vis= new boolean[V];
+    
         
-        visited[src]=true;
-        boolean ret=true;
-        for(Integer x:adj.get(src)){
-            if(x!=parent){
-                if(!visited[x])
-                ret=ret&&dfs(adj,src,x,visited);
-            else{
-                ret=false;
-                break;
-            }
+        for(int i=0;i<V;i++){
+            if(vis[i]==false){
+                if (checkCycle(i,-1,vis,adj))
+                    return true;
             }
         }
-        return ret;
+        
+        return false;
+        
+        
     }
-   
+    
+    public boolean checkCycle(int node, int parent,boolean[]vis,ArrayList<ArrayList<Integer>> adj ){
+        
+        if(vis[node])
+            return true;
+        
+        vis[node]=true;
+        
+        for(Integer x:adj.get(node)){
+            if(vis[x]==false){
+                if (checkCycle(x, node ,vis,adj)==true)
+                    return true;
+            }
+            else if(x!=parent)
+                return true;
+        }
+        return false;
+        
+        
+        
+    }
 }
