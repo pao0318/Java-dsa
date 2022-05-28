412be26480c3367869771c76099868ee7fd93d84
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-05-29 00:22:35 +0530

    Update reverse_recursion.java

--- single linked list/reverse_recursion.java
+++ single linked list/reverse_recursion.java
@@ -13,3 +13,57 @@ class Solution {
         
     }
 }
+
+// or
+import java.util.*;
+
+ class Node{
+	    int data;
+	    Node next;
+	    Node(int data){
+	        this.data=data;
+	        next=null;
+	    }
+	}
+
+public class Main
+{
+    static Node head;
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		Main list = new Main();
+		list.head= new Node(1);
+		list.head.next= new Node(2);
+		list.head.next.next= new Node(3);
+		list.head.next.next.next= new Node(4);
+		list.head.next.next.next.next= new Node(5);
+		
+		
+		head=list.reverse(head);
+		list.printList(head);
+	}
+	
+	public static Node reverse(Node head){
+	  
+	  if(head==null || head.next==null)
+	    return head;
+	    
+	  Node res=reverse(head.next);
+	  head.next.next=head;
+	  
+	  head.next=null;
+	  
+	  return res;
+	  
+	  
+	}
+	
+	public static void printList(Node head){
+	    while(head!=null){
+	        System.out.println(head.data);
+	        head=head.next;
+	}
+	
+
+}
+}
