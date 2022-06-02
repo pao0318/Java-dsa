38ed96141437b32463e4315ffa2fc737f025fcef
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-06-02 17:21:08 +0530

    Update merge_intervals.java

--- stacks/merge_intervals.java
+++ stacks/merge_intervals.java
@@ -7,7 +7,7 @@ class Solution {
         
         for(int i=0;i<intervals.length;i++){
             Interval top=stack.push(intervals[0]);
-//             Processing part 
+
             if(top.end<arr[i].start)
                     stack.push(arr[i]);
             
@@ -29,3 +29,45 @@ class Interval{
         this.end=end;
     }
 }
+
+
+// or Better
+class Solution {
+    public int[][] merge(int[][] intervals) {
+    Arrays.sort(intervals, (a,b)->(a[0]-b[0]));
+        ArrayList<int[]> list= new ArrayList<>();
+        
+        int start=intervals[0][0];
+        int end=intervals[0][1];
+        
+     
+        for(int i=1;i<intervals.length;i++){
+            int curr_start=intervals[i][0];
+            int curr_end=intervals[i][1];
+            
+            if(curr_start<=end ){
+                start=Math.min(start,curr_start);
+                end=Math.max(end,curr_end);
+            }
+            else{
+                list.add(new int[]{start,end});
+                start=curr_start;
+                end=curr_end;
+               
+            }
+        }
+        list.add(new int[]{start,end});
+        
+        int [][] res= new int[list.size()][2];
+        
+        for (int i=0;i<res.length;i++){
+            res[i][0]=list.get(i)[0];
+            res[i][1]=list.get(i)[1];
+        }
+        
+        return res;
+        
+        
+    }
+}
+   
