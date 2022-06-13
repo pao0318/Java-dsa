577b657213ffc198d445a222fe82e401148b8e42
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-06-14 00:13:50 +0530

    Create sort_recur.java

---
+++ recursion/sort_recur.java
@@ -0,0 +1,48 @@
+
+Kumkum Sharma
+1 year ago
+Thanks for amazing explanation  :)   Kudos to learning recursion. Enclosed java Solution:
+package recursion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortAnArray {
+
+	public static void main(String[] args) {
+		List<Integer> l = new ArrayList();
+		l.add(1);
+		l.add(0);
+		l.add(5);
+		l.add(2);
+		l = sort(l);
+		for(int i : l)
+			System.out.println(i);
+	}
+
+	private static List<Integer> sort(List<Integer> l) {
+		if(l.size() == 1)
+			return l;
+		//hypothesis
+		Integer temp = l.get(l.size()-1);
+		l.remove(l.size()-1);
+		l = sort(l);
+		//induction
+		return l = insert(l, temp);
+	}
+
+	private static List<Integer> insert(List<Integer> l, int i) {
+		if(l.size() == 0 || i >= l.get(l.size()-1))
+		{
+			l.add(i);
+			return l;
+		}
+		//hypothesis
+		int val = l.get(l.size() - 1);
+		l.remove(l.size()-1);
+		l = insert(l, i);
+		//induction
+		l.add(val);
+		return l;
+	}
+}
