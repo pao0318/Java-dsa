0212dc66e7d555f8f176acce5a43519e5a919153
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-06-14 22:52:44 +0530

    Create Bishoph.java

---
+++ codeforces/#799/Bishoph.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Bishop {
+
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+
+        int t= sc.nextInt();
+        while (t-->0){
+            int r=8,c=8;
+            char [][] board= new char[r][];
+            for(int i=0;i<8;i++)
+                board[i]=sc.next().toCharArray();
+
+            for(int x=1;x<7;x++){
+                for(int y=1;y<7;y++){
+                    if(board[x][y]=='#' && board[x+1][y+1]=='#' && board[x+1][y-1]=='#' && board[x-1][y-1]=='#')
+                        System.out.println(y+1+" "+ x+1);
+                }
+            }
+            
+
+            }
+
+    }
+}
