ce01a5e579eaa018a3ba884722073171cfe6a562
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-06-16 15:31:33 +0530

    Update Distinct_Palindrome.java

--- codechef/Div4#1/Distinct_Palindrome.java
+++ codechef/Div4#1/Distinct_Palindrome.java
@@ -1,28 +1,42 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+        while (t-->0){
+            int n=sc.nextInt();
+            int x=sc.nextInt();
+
+            if(n<(2*x)-1){
+                System.out.println(-1);
+                continue;
+            }
+            StringBuilder s=new StringBuilder();
+            for( int i=0;i<n;i++){
+                s.append('a');
+            }
 
 
+            for(int i=1;i<x;i++){
+                s.setCharAt(i-1,(char)('a'+i));
+                s.setCharAt(n-i,(char)('a'+i));
+            }
 
 
+            System.out.println(s);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
+	}
+}
 
 
 
