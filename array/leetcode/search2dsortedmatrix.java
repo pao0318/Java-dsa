5827018e4b201532701e9130baa54c34a7aee848
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-06-20 20:18:00 +0530

    Update search2dsortedmatrix.java

--- array/leetcode/search2dsortedmatrix.java
+++ array/leetcode/search2dsortedmatrix.java
@@ -1,19 +1,36 @@
 // https://leetcode.com/problems/search-a-2d-matrix-ii/
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        boolean flag=false;
-    
-        int col=matrix[0].length-1;
-        int i=0;
-        while(i<matrix.length &&col>=0){
-            if(matrix[i][col]>target)
-                col--;
-            else if(matrix[i][col]==target)
-                return true;
-            else
-                i++;
+import java.util.*;
+public class Search_in_sorted{
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int r=sc.nextInt();
+        int c= sc.nextInt();
+        int target= sc.nextInt();
+        int[][]arr= new int[r][c];
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++)
+                arr[i][j]=sc.nextInt();
         }
-        return false;
-        
+
+        boolean found=false;
+        int lo=0,hi=(r*c-1);
+        while(lo<=hi){
+            int mid=(lo+(hi-lo)/2);
+            if(target>arr[mid/c][mid%c])
+                lo=mid+1;
+            else if(target<arr[mid/c][mid%c])
+                hi=mid-1;
+            else{
+                found=true;
+                break;
+            }
+        }
+        System.out.println(found);
+
+
     }
+
+
+
 }
