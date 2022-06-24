9255671ca8d6d53dac33e8a95798e7d67326ddea
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-06-25 01:59:08 +0530

    Create detect_1st_loop.java

---
+++ single linked list/detect_1st_loop.java
@@ -0,0 +1,47 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+        class Node
+		{
+		    public int data;
+		    public Node next;
+
+		    Node(int data)
+		    {
+		        this.data = data;
+		        this.next = null;
+		    }
+		}
+
+*****************************************************************/
+
+public class Solution 
+{
+    public static Node firstNode(Node head) 
+    {
+         if(head==null)
+             return null;
+        Node slow=head;
+        Node fast=head;
+        Node start=head;
+    
+        
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                while(slow!=start){
+                    slow=slow.next;
+                    start=start.next;
+                }
+                return slow;
+            }
+                
+        }
+      return null;
+        
+    }
+}
