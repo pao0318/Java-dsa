a405704f87f13c8fced396b809e3efc0c23313c9
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-06-27 23:31:50 +0530

    Update contains_cycle_undirected.java

--- graph/contains_cycle_undirected.java
+++ graph/contains_cycle_undirected.java
@@ -1,3 +1,72 @@
+// Best method by union and path compression for finding parents
+
+import java.util.*;
+public class Disjoint_set{
+    public static void main(String []args){
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        int n=3;
+        for(int i=0;i<n;i++)
+            adj.add(new ArrayList<>());
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(0);
+        adj.get(2).add(0);
+        adj.get(1).add(2);
+        adj.get(2).add(1);
+
+        int[]parent= new int[n];
+        Arrays.fill(parent,-1);
+
+
+        for(int i=0;i<n;i++){
+            int src=adj.get(i).get(0);
+            int dest=adj.get(i).get(1);
+
+            int x=findPar(parent,src);
+            int y=findPar(parent,dest);
+            if(x==y){
+                System.out.println("Graph contains cycle");
+                break;
+            }
+            parent[x]=y;
+        }
+
+    }
+
+    public static int findPar(int[]parent, int i){
+        if(parent[i]==-1)
+            return i;
+        return findPar(parent, parent[i]);
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// OR
+
+
+
+
+
+
 class Solution {
     // Function to detect cycle in an undirected graph.
     public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
