5fc23abec428dad6bd19a572eea6b2458e737b1b
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-06-29 14:52:57 +0530

    Update word_break2.java

--- recursion/word_break2.java
+++ recursion/word_break2.java
@@ -1,3 +1,45 @@
+// https://www.codingninjas.com/codestudio/problems/983635?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=1
+
+import java.util.stream.Collectors;
+import java.util.*;
+import java.lang.*;
+
+public class Solution {
+
+	public static ArrayList<String> wordBreak(String s, ArrayList<String> dictionary) {
+		ArrayList<String> list= new ArrayList<>();
+        
+        helper(0,s,dictionary,list, new LinkedList<String>());
+        
+        return list;
+	}
+    
+    public static void helper(int ind, String s,ArrayList<String> dictionary,ArrayList<String> list,
+                            LinkedList<String>t ){
+        if(ind==s.length()){
+          list.add(t.stream().collect(Collectors.joining(" ")));
+            return;
+        }
+        
+        for(int i=ind;i<s.length();i++){
+            String str=s.substring(ind,i+1);
+            if(dictionary.contains(str)){
+                t.add(str);
+                helper(i+1, s,dictionary,list,t);
+                t.removeLastOccurrence(str);
+            }
+            
+        }
+
+        
+        
+    }
+}
+
+
+// Or GFG WALA
+
+
 // https://practice.geeksforgeeks.org/problems/word-break-part-23249/1#
 
 class Solution{
