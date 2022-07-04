3907c6ec34ff3ce3f535ae0a099541093537c20c
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-04 18:14:59 +0530

    Solved!!!!!

--- doubts/allocate_minimum_pages.java
+++ doubts/allocate_minimum_pages.java
@@ -4,15 +4,43 @@ class Solution
     //Function to find minimum number of pages.
     public static int findPages(int[]A,int N,int M)
     {
-         if(M>=N)
-            return -1;
-        int sum=0;
-        int k=N-M+1;
-        for(int i=0;i<k;i++)
-            sum+=A[i];
+        int lo=Integer.MIN_VALUE;
+        int high=0;
+        int res=-1;
+        for(int k:A){
+             high+=k;
+             lo=Math.max(lo,k);
+        }
         
-       
-        return sum;
+        while(lo<=high){
+            int mid=(high-(high-lo)/2);
+            if(isValid(mid,A,M,N)){
+                res=mid;
+                high=mid-1;
+            }
+            else
+                lo=mid+1;
+                
+        }
+        return res;
+        
+    }
+    
+    
+    public static boolean isValid(int mid, int []A, int M, int N){
+        
+        int curr_sum=0;
+        int students=1;
+        for(int i=0;i<N;i++){
+            if((curr_sum+A[i])<=mid)
+                curr_sum+=A[i];
+            else{
+                students++;
+                curr_sum=A[i];
+            }
+        }
+        return students<=M;
+        
         
     }
 };
