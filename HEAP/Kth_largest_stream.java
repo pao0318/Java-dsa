272ed092eaf18b18deb3ca066374e7c96e671188
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-06 14:19:35 +0530

    Kth largest number

--- HEAP/Kth_largest_stream.java
+++ HEAP/Kth_largest_stream.java
@@ -1,23 +1,22 @@
 import java.util.*;
+public class kth_largest {
 
-class KthLargestStream {
-    PriorityQueue<Integer> pq=new PriorityQueue<>();
-    int k;
-    public KthLargestStream(int[] nums, int k) {
-        this.k=k;
-        for(int i=0;i<nums.length;i++){
-            pq.add(nums[i]);
-            if(pq.size()>k+1)
-                pq.poll();
-        }         
+    public static void main(String args[]){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        int k= sc.nextInt();
+        k=k-1;
+        int[]arr= new int[n];
+        for(int i=0;i<n;i++) arr[i]=sc.nextInt();
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0;i<n;i++) pq.add(arr[i]);
 
-    }
+        if(k<n){
+            while(k-->0)
+                pq.remove();
+        }
 
-    public int add(int val) {
-        pq.add(val);
-        if(pq.size()>k+1)
-            pq.poll();
-        return pq.peek();    
-        
+        System.out.println(pq.peek());
+
     }
 }
