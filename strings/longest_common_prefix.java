4cb3b0125e3a12d4cb4717ef58bb681f80f6b7ed
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-13 14:18:06 +0530

    Update longest_common_prefix.java

--- strings/longest_common_prefix.java
+++ strings/longest_common_prefix.java
@@ -1,3 +1,5 @@
+https://www.codingninjas.com/codestudio/problems/2090383?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=1
+// 
 import java.util.*;
 
 class Solution {
@@ -17,4 +19,51 @@ class Solution {
         return res;
 
     }
+}
+
+// nAYA WALA
+import java.util.*;
+public class Solution {
+    public static int findmin(String[]arr, int n){
+        int minindex=Integer.MAX_VALUE;
+        for(int i=0;i<arr.length;i++)
+            minindex=Math.min(minindex, arr[i].length());
+        return minindex;
+    }
+    
+    public static boolean allContains(String []arr, int n,String small,int low,int high){
+        for(int i=0;i<n;i++){
+            String temp=arr[i];
+            
+            for(int j=low;j<=high;j++){
+                if(small.charAt(j)!=temp.charAt(j))
+                    return false;
+            }
+           
+        }
+         return true;
+    }
+    
+	public static String longestCommonPrefix(String[] arr, int n) {
+	    int index=findmin(arr,n);
+        String prefix="";
+        int low=0;
+        int high=index-1;
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(allContains(arr,n,arr[0],low,mid)){
+                prefix+=arr[0].substring(low,mid+1);
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        
+       return prefix;
+        
+    }
+    
+
 }
