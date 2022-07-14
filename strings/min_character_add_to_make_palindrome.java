e2fcc69b30fb43cb1383d6470c1dd90ec8ee85b9
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-14 13:33:01 +0530

    Create min_character_add_to_make_palindrome.java

---
+++ strings/min_character_add_to_make_palindrome.java
@@ -0,0 +1,32 @@
+// https://www.codingninjas.com/codestudio/problems/893000?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+
+public class Solution {
+    static boolean ispalindrome(String s) {
+        int l = s.length();
+ 
+        for (int i = 0, j = l - 1; i <= j; i++, j--) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int minCharsforPalindrome(String str) {
+        int cnt=0;
+        boolean flag=false;
+        
+        while(str.length()>0){
+            if(ispalindrome(str)){
+                flag=true;
+                break;
+            }
+            else{
+                cnt++;
+                str=str.substring(0,str.length()-1);
+            }
+        }
+        return cnt;
+    }
+
+}
