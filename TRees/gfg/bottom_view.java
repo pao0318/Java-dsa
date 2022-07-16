f612bbafec98918da4adf9bae685869c0eb9a337
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-17 01:42:10 +0530

    Update bottom_view.java

--- TRees/gfg/bottom_view.java
+++ TRees/gfg/bottom_view.java
@@ -1,46 +1,48 @@
 // https://practice.geeksforgeeks.org/problems/bottom-view-of-binary-tree/1#
 
+class Pair{
+    Node node;
+    int level;
+    Pair(Node node, int level){
+        this.node=node;
+        this.level=level;
+    }
+}
+
 class Solution
 {
     //Function to return a list containing the bottom view of the given tree.
     public ArrayList <Integer> bottomView(Node root)
     {
-        if(root==null)
-            return null;
-            
-       ArrayList<Integer>list=new ArrayList<>();
-       
-       Queue<Pair> pq=new LinkedList<Pair>();
-       TreeMap<Integer,Integer> map=new TreeMap<>();
-       
-       pq.add(new Pair(root,0));
-        while(!pq.isEmpty()){
-            Pair p=pq.poll();
-            Node curr=p.node;
-            int dist=p.hd;
-            
-            map.put(dist,curr.data);
-    
-            if(curr.left!=null)
-                pq.add(new Pair(curr.left,dist-1));
-                
-                
-            if(curr.right!=null)
-                pq.add(new Pair(curr.right,dist+1));
-
-        }
-        for(Map.Entry<Integer,Integer>x:map.entrySet())
-            list.add(x.getValue());
-            return list;
+        ArrayList<Integer> list= new ArrayList<>();
         
-       
-    }
-}
-class Pair{
-    Node node;
-    int hd;
-    public Pair(Node temp,int d){
-        this.node=temp;
-        this.hd=d;
+        Map<Integer, Integer> map = new TreeMap<>();
+        Queue<Pair>pq= new LinkedList<>();
+        
+        pq.add(new Pair(root,0));
+        while(!pq.isEmpty()){
+   
+            Pair p=pq.poll();
+            Node temp=p.node;
+            int val=p.level;
+            map.put(val,temp.data);
+           
+           if(temp.left!=null)
+                pq.add(new Pair(temp.left,val+1));
+            if(temp.right!=null)
+                pq.add(new Pair(temp.right,val-1));  
+          
+            
+        }
+        
+        
+        for(Map.Entry<Integer,Integer> x:map.entrySet()){
+            list.add(x.getValue());
+        }
+        Collections.reverse(list);
+        return list;
+
+        
+        
     }
 }
