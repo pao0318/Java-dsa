aa2bb08e89157349b409b6c2d8e0eb2ffc484e83
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-16 23:44:13 +0530

    Update post_order_stack.java

--- TRees/gfg/post_order_stack.java
+++ TRees/gfg/post_order_stack.java
@@ -47,3 +47,51 @@ class Main{
     
     
 }
+
+// Or
+// https://leetcode.com/problems/binary-tree-postorder-traversal/submissions/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        
+        List<Integer> res= new ArrayList<>();
+        Stack<TreeNode> stack= new Stack<>();
+     
+        while(!stack.isEmpty()||root!=null){
+            if(root!=null){
+                stack.add(root);
+                root=root.left;
+            }
+            else{
+                TreeNode temp=stack.peek().right;
+                if(temp==null){
+                    temp=stack.pop();
+                    res.add(temp.val);
+                    
+                    while(!stack.isEmpty() && temp==stack.peek().right){
+                        temp=stack.pop();
+                        res.add(temp.val);
+                    }
+                }
+                else
+                    root=temp;
+            }
+        }
+        return res;
+            
+    }
+}
