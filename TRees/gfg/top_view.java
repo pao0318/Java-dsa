263e5e7dee6cd580182faadf295c47e2892c8fe8
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-17 01:59:03 +0530

    Update top_view.java

--- TRees/gfg/top_view.java
+++ TRees/gfg/top_view.java
@@ -1,39 +1,48 @@
+// https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1 
 class Pair{
     Node node;
-    int hd;
-    Pair(Node temp, int d){
-        node=temp;
-        hd=d;
+    int level;
+    Pair(Node node, int level){
+        this.node=node;
+        this.level=level;
     }
 }
 class Solution
 {
     //Function to return a list of nodes visible from the top view 
     //from left to right in Binary Tree.
-    
     static ArrayList<Integer> topView(Node root)
     {
-        ArrayList<Integer> list=new ArrayList<>();
-        TreeMap<Integer,Integer> map=new TreeMap<>();
-        Queue<Pair>pq=new LinkedList<Pair>();
+         ArrayList<Integer> list= new ArrayList<>();
+        
+        Map<Integer,Integer> map = new TreeMap<>();
+        Queue<Pair>pq= new LinkedList<>();
         
         pq.add(new Pair(root,0));
+        
         while(!pq.isEmpty()){
             Pair p=pq.poll();
-            Node curr=p.node;
-            int point=p.hd;
-            if(!map.containsKey(point))
-                map.put(point,curr.data);
-            if(curr.left!=null)  
-                pq.add(new Pair(curr.left,point-1));
-            if(curr.right!=null)
-                pq.add(new Pair(curr.right,point+1));
-        }
-        for(Map.Entry<Integer,Integer>x:map.entrySet())
-            list.add(x.getValue());
             
-        return list;    
-          
-         
+            Node temp=p.node;
+            int level=p.level;
+            
+            if(!map.containsKey(level))
+                map.put(level,temp.data);
+            
+            if(temp.left!=null)
+                pq.add(new Pair(temp.left,level+1));
+            if(temp.right!=null)
+                pq.add(new Pair(temp.right,level-1));  
+            
+        }
+        
+   for(Map.Entry<Integer,Integer> x: map.entrySet()){
+       list.add(x.getValue());
+   }
+   Collections.reverse(list);
+   
+   return list;
     }
+ 
+    
 }
