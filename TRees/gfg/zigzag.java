813d7b2df49e3025322d509f7ad88dfce94a79ad
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-17 14:26:38 +0530

    Update zigzag.java

--- TRees/gfg/zigzag.java
+++ TRees/gfg/zigzag.java
@@ -33,3 +33,64 @@ class GFG
 	    
 	}
 }
+
+// Leetcode 
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/submissions/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        
+        if(root==null)
+            return new ArrayList<>();
+        List<List<Integer>> res= new ArrayList<>();
+        Queue<Pair> pq= new LinkedList<>();
+        pq.add(new Pair(root,0));
+        
+        boolean flag=false;
+        while(!pq.isEmpty()){
+            int size=pq.size();
+            int level=pq.peek().level;
+            List<Integer> list= new ArrayList<>();
+            
+            for(int i=0;i<size;i++){
+                Pair it=pq.poll();
+                TreeNode temp=it.node;
+                
+                if(temp.left!=null)
+                    pq.offer(new Pair(temp.left,level+1));
+                if(temp.right!=null)
+                    pq.offer(new Pair(temp.right,level+1));
+                 list.add(temp.val);
+            }
+            if(flag)
+                Collections.reverse(list);
+            flag=!flag;
+            res.add(list);
+           
+        }
+        return res;
+        
+    }
+}
+class Pair{
+    TreeNode node;
+    int level;
+    Pair(TreeNode node, int level){
+        this.node=node;
+        this.level=level;
+    }
+}
