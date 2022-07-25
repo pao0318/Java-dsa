5f88118356b1aa93936d7c5820fd5a6b2e6fad3b
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-26 01:49:26 +0530

    Update edit_distance.java

--- DP/gfg/edit_distance.java
+++ DP/gfg/edit_distance.java
@@ -1,28 +1,27 @@
 // https://practice.geeksforgeeks.org/problems/edit-distance3702/1
-class Solution {
-    public int editDistance(String s, String t) {
+public class Solution {
+    public int minDistance(String A, String B) {
+        int n=A.length();
+        int m=B.length();
         
-        int n=s.length();
-        int m=t.length();
-        int[][]dp=new int[n+1][m+1];
-        for(int i=0;i<n+1;i++){
-        for(int j=0;j<m+1;j++){
-            if(i==0)
-                dp[i][j]=j;
-            if(j==0)
-                dp[i][j]=i;
-        }
-        }
+        int[][]dp= new int[n+1][m+1];
+        
+        for(int i=0;i<n+1;i++)
+            dp[i][0]=i;
+        
+        for(int i=0;i<m+1;i++)
+            dp[0][i]=i;
         
         for(int i=1;i<n+1;i++){
             for(int j=1;j<m+1;j++){
-            if(s.charAt(i-1)==t.charAt(j-1))
-                dp[i][j]=dp[i-1][j-1];
-            else{
-                dp[i][j]=1+Math.min(dp[i-1][j],Math.min(dp[i][j-1], dp[i-1][j-1]));
+                if(A.charAt(i-1)==B.charAt(j-1))
+                    dp[i][j]=dp[i-1][j-1];
+                else
+                    dp[i][j]=1+Math.min(dp[i-1][j-1],Math.min(dp[i][j-1],dp[i-1][j]));
+                
             }
-        }   
         }
         return dp[n][m];
     }
 }
+
