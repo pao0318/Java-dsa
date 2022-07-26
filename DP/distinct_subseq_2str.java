9d0a745a5a7c4ad0870a5b80e43a85c6b96fdaa8
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-26 11:54:11 +0530

    Update distinct_subseq_2str.java

--- DP/distinct_subseq_2str.java
+++ DP/distinct_subseq_2str.java
@@ -1,4 +1,26 @@
 // https://www.interviewbit.com/problems/distinct-subsequences/
+
+// Recursion
+public class Solution {
+    public int numDistinct(String A, String B) {
+        int n=A.length();
+        int m=B.length();
+        
+        return recur(A,B,n-1,m-1);
+    }
+    public int recur(String A, String B, int i, int j){
+        if(i<0)
+            return 0;
+        if(j<0)
+            return 1;
+        if(A.charAt(i)==B.charAt(j))
+            return recur(A,B,i-1,j-1)+recur(A,B,i-1,j);
+        else
+            return recur(A,B,i-1,j);
+    }
+}
+
+// DP
 public class Solution {
     public int numDistinct(String A, String B) {
         int n=A.length();
