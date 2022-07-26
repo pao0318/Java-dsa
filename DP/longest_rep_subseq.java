3362c9f1bbafcddf34c5646b31c2ee357493df66
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-07-26 11:18:27 +0530

    Update longest_rep_subseq.java

--- DP/longest_rep_subseq.java
+++ DP/longest_rep_subseq.java
@@ -23,3 +23,20 @@ public class Solution {
 
 // DP
 
+public class Solution {
+    public int anytwo(String A) {
+          int n=A.length();
+        String S=A;
+        int[][]dp= new int[n+1][n+1];
+        for(int i=1;i<n+1;i++) {
+            for (int j = 1; j < n+1; j++) {
+                if (A.charAt(i - 1) == S.charAt(j - 1) && i != j)
+                    dp[i][j]=1+dp[i-1][j-1];
+                else
+                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+            }
+
+        }
+        return dp[n][n]>=2?1:0;
+    }
+}
