3512a80122ddb562a2c39c8eca65ca54ebe1a603
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2022-08-06 22:23:08 +0530

    Update rotate_matrix90.java

--- array/rotate_matrix90.java
+++ array/rotate_matrix90.java
@@ -3,19 +3,52 @@ import java.util.*;
 
 class Solution {
     public int[][] solve(int[][] matrix) {
-        int R = matrix.length;
-        int C = matrix[0].length;
-        int[][] ans = new int[C][R];
-        int k = 0;
-        int p = 0;
-        for (int i = 0; i < R; i++) {
-            k = i;
-            p = 0;
-            for (int j = C - 1; j >= 0; j--) {
-                ans[p][k] = matrix[i][j];
-                p++;
+        // reversing rows
+        for(int[]row:matrix)
+            reverse(row);
+        
+        // Transposing the matrix;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-        return ans;
+        return matrix;
+        
+    }
+
+    public void reverse(int[]row){
+        int start=0;
+        int end=row.length-1;
+
+        while(start<end){
+            int temp=row[start];
+            row[start]=row[end];
+            row[end]=temp;
+            start++;
+            end--;
+        }
+    }
+}
+
+// OR with O(n^2) space Complexity
+import java.util.*;
+
+class Solution {
+    public int[][] solve(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int[][] res = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                res[i][j] = matrix[j][m - i - 1];
+            }
+        }
+        return res;
     }
 }
