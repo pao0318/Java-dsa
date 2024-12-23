374b75584c4a909b7dcccb8f74141a6ecca15615
Siddharth Sharma <70106499+pao0318@users.noreply.github.com>
2024-12-24 00:49:36 +0530

    Update coin_sum.java

--- DP/coin_sum.java
+++ DP/coin_sum.java
@@ -1,35 +1,40 @@
+//package com.example.demo;
+import java.io.*;
 import java.util.*;
-public class Main{
-    public static void main(String args[]) throws Exception{
-        int arr[]={1,2,3};
-        int sum=5;
-        int ans=solve(arr,sum);
-        System.out.println(ans);
-        }
-    static int solve(int[]coin,int sum){
-        int n=coin.length;
-        int[][]dp=new int[n+1][sum+1];
-        // Initialization
-        for(int i=0;i<n+1;i++){
-            for(int j=0;j<sum+1;j++){
-                if(i==0)
-                    dp[i][j]=0;
-                if(j==0&&i>0)
-                    dp[i][j]=1;
-        }
-        
-    }
-    // processing
-    for(int i=1;i<n+1;i++){
-            for(int j=1;j<sum+1;j++){
-                if(coin[i-1]<=j)
-                    dp[i][j]=dp[i][j-coin[i-1]]+dp[i-1][j];
-                else
-                    dp[i][j]=dp[i-1][j];
-        }
-        
-    }
-    
-    return dp[n][sum];
-}
-}
+import java.util.stream.Collectors;
+
+
+public class DemoApplication {
+	static final int MOD = 1000000007;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		// Read inputs
+
+		String[] s = br.readLine().split(" ");
+		int n = Integer.parseInt(s[0]);
+
+		int target = Integer.parseInt(s[1]);
+
+		List<Integer> input = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+		int[] dp = new int[target+1];
+
+		dp[0]=1;
+
+		for(int i=1;i<=target;i++){
+			for(int j=0;j<input.size();j++){
+				if(i-input.get(j)>=0)
+					dp[i]= (dp[i]+dp[i-input.get(j)])%MOD;
+			}
+
+		}
+		System.out.println(dp[target]);
+
+
+
+	}
+
+
+
